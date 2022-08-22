b3c6dbc668c7574d76b639cb888258d68f559fef
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-08-22 16:11:17 +0200

    Improve routine to download new versions

--- src/routines/__init__.py
+++ src/routines/__init__.py
@@ -1,2 +1,3 @@
 from . import check_software_dependencies
+from . import manage_local_files
 from . import repository_interface
---
+++ src/routines/manage_local_files.py
@@ -0,0 +1,32 @@
+import os
+from src.utils import directory_utils, shell_utils
+
+
+def download_release(release_tag: str) -> None:
+    """
+    For a given release tag "vX.Y.Z", download
+    the code and its ui-installer.
+    """
+    pyra_dir = os.path.join(directory_utils.get_documents_dir(), "pyra")
+    tar_name = f"pyra-{release_tag[1:]}.tar.gz"
+    ui_installer_name = f"Pyra.UI_{release_tag[1:]}_x64_en-US.msi"
+
+    # download codebase tarball, extract code, and remove tarball
+    shell_utils.run_shell_command(
+        f"gh release download --repo tum-esm/pyra --archive=tar.gz {release_tag}",
+        cwd=pyra_dir,
+    )
+    shell_utils.run_shell_command(f"tar -xf {tar_name}", cwd=pyra_dir)
+    shell_utils.run_shell_command(f"rm {tar_name}", cwd=pyra_dir)
+
+    # download ui installer, and move it to pyra/ui-installers
+    shell_utils.run_shell_command(
+        f"gh release download --repo tum-esm/pyra {release_tag}",
+        cwd=pyra_dir,
+    )
+    os.rename(
+        os.path.join(pyra_dir, ui_installer_name),
+        os.path.join(pyra_dir, "ui-installer", ui_installer_name),
+    )
+
+    # TODO: open the eplorer at that location using 'explorer ...' (windows) and 'open ...' (unix)
