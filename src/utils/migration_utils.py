cbef1f99e0301975547d81f4e994fb92da0f59c0
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-01-04 16:31:58 +0100

    Add migration routine from 4.0.6 -> 4.0.7

--- src/utils/migration_utils.py
+++ src/utils/migration_utils.py
@@ -7,6 +7,7 @@ def run(from_dict: Any, from_version: str) -> tuple[Any, str]:
         to_version = {
             "4.0.4": "4.0.5",
             "4.0.5": "4.0.6",
+            "4.0.6": "4.0.7",
         }[from_version]
     except KeyError:
         raise Exception(f'Unknown version "{from_version}"')
@@ -26,18 +27,23 @@ def run(from_dict: Any, from_version: str) -> tuple[Any, str]:
             del to_dict["vbdsd"]
 
             if to_dict["helios"] is not None:
-                to_dict["helios"]["edge_detection_threshold"] = 0.02
+                to_dict["helios"]["edge_detection_threshold"] = 0.01
 
             to_dict["upload"] = None
 
         if to_version == "4.0.6":
             pass
 
+        if to_version == "4.0.7":
+            if to_dict["helios"] is not None:
+                del to_dict["helios"]["measurement_threshold"]
+
         # add future migration rules here
 
     except Exception as e:
         raise Exception(
-            f"Could not perform config migration {from_version} -> {to_version}: {e}"
+            f"Could not perform config migration " +
+            f"{from_version} -> {to_version}: {e}"
         )
 
     return to_dict, to_version
