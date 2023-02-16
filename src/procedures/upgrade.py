939d50808ba4db20ba72d21791a8f148bba23e3f
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-02-16 12:02:51 +0100

    Move command procedures into subdirectory (1)
    upgrade.py

---
+++ src/procedures/upgrade.py
@@ -0,0 +1,45 @@
+from src import utils, routines
+
+
+def run() -> None:
+    local_pyra_versions = routines.find_versions.get_local_versions()
+    remote_pyra_versions = routines.find_versions.get_remote_versions()
+    if len(remote_pyra_versions) == 0:
+        print("Did not find any remote pyra versions.")
+        return
+
+    version_to_be_installed = utils.printing_utils.pretty_input(
+        f"Which version should be installed?", remote_pyra_versions
+    )
+    if version_to_be_installed not in remote_pyra_versions:
+        utils.printing_utils.pretty_print(f'Invalid version "{version_to_be_installed}"')
+        return
+
+    if version_to_be_installed in local_pyra_versions:
+        utils.printing_utils.pretty_print(
+            f'Please uninstall the local "{version_to_be_installed}" first'
+        )
+        return
+
+    routines.manage_local_files.download_version(version_to_be_installed)
+    routines.installation.install_version(version_to_be_installed)
+
+    available_versions_to_migrate_from = routines.find_versions.get_versions_to_migrate_from(
+        version_to_be_installed
+    )
+    if len(available_versions_to_migrate_from) == 0:
+        print("Skipping migration, no available versions to migrate from")
+    else:
+        version_to_migrate_from = utils.printing_utils.pretty_input(
+            f"Should we reuse the config.json from a previously installed version?",
+            [
+                "no",
+                *available_versions_to_migrate_from,
+            ],
+        )
+        if version_to_migrate_from != "no":
+            routines.installation.migrate_config(
+                version_to_migrate_from, version_to_be_installed
+            )
+
+    utils.printing_utils.pretty_print("done!", color="green")
