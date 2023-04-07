fa8180ac32737001953a802abf0f45c93fe9fc1c
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-04-07 15:34:54 +0200

    Add tests (4)
    split check environment checks

--- tests/test_check_environment.py
+++ tests/test_check_environment.py
@@ -1,8 +1,11 @@
 from src import tasks
 
 
-def test_environment() -> None:
+def test_check_python_version() -> None:
     tasks.check_environment.check_python_version()
+
+
+def test_check_command_availability() -> None:
     tasks.check_environment.check_command_availability("poetry")
     tasks.check_environment.check_command_availability("curl")
     tasks.check_environment.check_command_availability("tar")
