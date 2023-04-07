c10e6a96b75b1490c05eb1de4976cff5395cfc9b
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-04-07 15:41:55 +0200

    Add tests (5)
    test find versions

--- tests/test_download_version.py
+++ tests/test_download_version.py
@@ -9,7 +9,7 @@ def test_download_version(clear_local_pyra_dir: None) -> None:
 
     # TODO: do this for all found releases
 
-    for v in ["4.0.5", "4.0.6", "4.0.7"]:
+    for v in ["4.0.6", "4.0.7"]:
         code_dir = os.path.join(pyra_dir, f"pyra-{v}")
         ui_installers_file = os.path.join(
             pyra_dir,
@@ -21,8 +21,10 @@ def test_download_version(clear_local_pyra_dir: None) -> None:
         assert not os.path.isfile(
             ui_installers_file
         ), f'file "{ui_installers_file}" should not exist'
+        assert v not in [lv.as_str() for lv in tasks.find_versions.get_local_versions()]
 
         tasks.download_version.download_version(Version(v))
 
         assert os.path.isdir(code_dir), f'directory "{code_dir}" should exist'
         assert os.path.isfile(ui_installers_file), f'file "{ui_installers_file}" should exist'
+        assert v in [lv.as_str() for lv in tasks.find_versions.get_local_versions()]
