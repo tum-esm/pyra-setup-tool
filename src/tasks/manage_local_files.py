f9462de6ec380cd0c1cc7d22fe6d90ceac93dec1
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-04-07 13:29:08 +0200

    Remove GitHub-CLI dependency (2)
    download code and assets with curl

--- src/tasks/manage_local_files.py
+++ src/tasks/manage_local_files.py
@@ -4,48 +4,42 @@ from src.utils import directory_utils, printing_utils, shell_utils
 
 
 def download_version(version: str) -> None:
-    """
-    For a given release version "x.y.z", download
-    the code and its ui-installer.
-    """
-    pyra_dir = os.path.join(directory_utils.get_documents_dir(), "pyra")
-    tar_name = f"pyra-{version}.tar.gz"
-    ui_installer_name = f"Pyra.UI_{version}_x64_en-US.msi"
+    """For a given release version "x.y.z", download
+    the code and its ui-installer."""
 
-    # download codebase tarball, extract code, and remove tarball
+    pyra_dir = os.path.join(directory_utils.get_documents_dir(), "pyra")
+
+    # download codebase tarball and extract code
     shell_utils.run_shell_command(
-        f"gh release download --repo tum-esm/pyra --archive=tar.gz v{version}",
+        f"curl -L https://github.com/tum-esm/pyra/archive/refs/tags/"
+        + f"v{version}.tar.gz | tar zx",
         cwd=pyra_dir,
     )
-    shell_utils.run_shell_command(f"tar -xf {tar_name}", cwd=pyra_dir)
-    os.remove(os.path.join(pyra_dir, tar_name))
     printing_utils.pretty_print("Downloaded code", color="green")
 
-    # download ui installer, and move it to pyra/ui-installers
+    # download ui installer
     shell_utils.run_shell_command(
-        f"gh release download --repo tum-esm/pyra v{version}",
-        cwd=pyra_dir,
-    )
-    os.rename(
-        os.path.join(pyra_dir, ui_installer_name),
-        os.path.join(pyra_dir, "ui-installers", ui_installer_name),
+        f'curl -L -O "https://github.com/tum-esm/pyra/releases/download'
+        + f'/v{version}/Pyra.UI_{version}_x64_en-US.msi"',
+        cwd=os.path.join(pyra_dir, "ui-installers"),
     )
     printing_utils.pretty_print("Downloaded UI", color="green")
 
 
 def remove_version(version: str) -> None:
-    """
-    For a given release version "x.y.z", remove
-    the code and its ui-installer.
-    """
-    pyra_dir = os.path.join(directory_utils.get_documents_dir(), "pyra")
-    ui_installer_path = os.path.join(
-        pyra_dir, "ui-installers", f"Pyra.UI_{version}_x64_en-US.msi"
-    )
-    code_dir = os.path.join(pyra_dir, f"pyra-{version}")
+    """For a given release version "x.y.z", remove
+    the code and its ui-installer."""
 
+    pyra_dir = os.path.join(directory_utils.get_documents_dir(), "pyra")
+
+    # remove codebase if it exists
+    code_dir = os.path.join(pyra_dir, f"pyra-{version}")
     if os.path.isdir(code_dir):
         shutil.rmtree(code_dir)
 
+    # remove ui installer if it exists
+    ui_installer_path = os.path.join(
+        pyra_dir, "ui-installers", f"Pyra.UI_{version}_x64_en-US.msi"
+    )
     if os.path.isfile(ui_installer_path):
         os.remove(ui_installer_path)
