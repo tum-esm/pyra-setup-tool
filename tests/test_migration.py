05b1bf234038fa29d2189eb109f89c62fdb1872c
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-12-05 23:52:38 +0100

    Test migration (2)
    Add test script

--- tests/test_migration.py
+++ tests/test_migration.py
@@ -1,11 +1,42 @@
+import json
+import tum_esm_utils
+import deepdiff
+from src import Version, tasks
 
-from src import Version
 
 def test_migration() -> None:
     for from_version in [
         Version("4.0.5"),
         Version("4.0.6"),
         Version("4.0.7"),
-        Version("4.0.8")
+        #Version("4.0.8")
     ]:
-        pass
+        src_config = tum_esm_utils.files.load_json_file(
+            tum_esm_utils.files.rel_to_abs_path(
+                f"./configs/pyra-config-{from_version.tag[1:]}.json"
+            )
+        )
+        for to_version in [
+            Version("4.0.6"),
+            Version("4.0.7"),
+            Version("4.0.8"),
+            #Version("4.1.0"),
+        ]:
+            if to_version < from_version:
+                continue
+            print(f"Testing migration from {from_version} to {to_version}")
+            dst_config = tum_esm_utils.files.load_json_file(
+                tum_esm_utils.files.rel_to_abs_path(
+                    f"./configs/pyra-config-{to_version.tag[1:]}.json"
+                )
+            )
+            migrated_src_config = tasks.migrate_config.migrate_config_object(
+                src_config, from_version, to_version
+            )
+            difference = deepdiff.DeepDiff(
+                migrated_src_config, dst_config, ignore_order=True
+            )
+            print(f"difference = {json.dumps(difference, indent=4)}")
+            assert len(
+                difference
+            ) == 0, f"Migration from {from_version} to {to_version} failed."
