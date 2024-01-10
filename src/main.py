252e007c8d2c17a90e265ec03d5def230bccffa0
Moritz Makowski <moritz@dostuffthatmatters.dev>
2024-01-10 10:05:33 +0100

    #9 Migrate from Poetry to PDM (3)
    Remove checks for Poetry existence

--- src/main.py
+++ src/main.py
@@ -46,7 +46,6 @@ def run() -> None:
             return
 
         tasks.check_environment.check_python_version()
-        tasks.check_environment.check_command_availability("poetry")
         tasks.check_environment.check_command_availability("curl")
         tasks.check_environment.check_command_availability("tar")
         tasks.check_environment.check_command_availability("git")
--- tests/test_check_environment.py
+++ tests/test_check_environment.py
@@ -6,7 +6,6 @@ def test_check_python_version() -> None:
 
 
 def test_check_command_availability() -> None:
-    tasks.check_environment.check_command_availability("poetry")
     tasks.check_environment.check_command_availability("curl")
     tasks.check_environment.check_command_availability("tar")
     tasks.check_environment.check_command_availability("git")
