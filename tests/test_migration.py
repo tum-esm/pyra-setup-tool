7e899e77abc2d6ad5a5265758e7f6660e827ab9a
Moritz Makowski <moritz@dostuffthatmatters.dev>
2025-02-22 19:47:57 +0100

    Test migration with both minimal and full configs

--- tests/test_migration.py
+++ tests/test_migration.py
@@ -17,29 +17,31 @@ ALL_VERSIONS = [
 
 
 def test_migration() -> None:
-    for from_version in ALL_VERSIONS:
-        src_config = tum_esm_utils.files.load_json_file(
-            tum_esm_utils.files.rel_to_abs_path(
-                f"./configs/pyra-config-{from_version.tag[1:]}.json"
-            )
-        )
-        for to_version in ALL_VERSIONS:
-            if to_version <= from_version:
-                continue
-            print(f"Testing migration from {from_version} to {to_version}")
-            dst_config = tum_esm_utils.files.load_json_file(
+    for variant in ["-minimal", "-full"]:
+        for from_version in ALL_VERSIONS:
+            src_config = tum_esm_utils.files.load_json_file(
                 tum_esm_utils.files.rel_to_abs_path(
-                    f"./configs/pyra-config-{to_version.tag[1:]}.json"
+                    f"./configs/pyra-config-{from_version.tag[1:]}{variant}.json"
                 )
             )
-            migrated_src_config = tasks.migrate_config.migrate_config_object(
-                src_config, from_version, to_version
-            )
-            difference = deepdiff.DeepDiff(
-                migrated_src_config,
-                dst_config,
-                ignore_order=True,
-                ignore_numeric_type_changes=True,
-            )
-            print(f"difference = {difference.to_json(indent=4)}")
-            assert len(difference) == 0, f"Migration from {from_version} to {to_version} failed."
+            for to_version in ALL_VERSIONS:
+                if to_version <= from_version:
+                    continue
+                print(f"Testing migration from {from_version} to {to_version}")
+                dst_config = tum_esm_utils.files.load_json_file(
+                    tum_esm_utils.files.rel_to_abs_path(
+                        f"./configs/pyra-config-{to_version.tag[1:]}{variant}.json"
+                    )
+                )
+                migrated_src_config = tasks.migrate_config.migrate_config_object(
+                    src_config, from_version, to_version
+                )
+                difference = deepdiff.DeepDiff(
+                    migrated_src_config,
+                    dst_config,
+                    ignore_order=True,
+                    ignore_numeric_type_changes=True,
+                )
+                assert len(difference) == 0, (
+                    f"Migration from {from_version} to {to_version} failed."
+                )
