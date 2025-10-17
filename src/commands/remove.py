2028a1b563b363eb300e8e9d58f1f445d78f565c
Moritz Makowski <moritz.makowski@tum.de>
2025-10-17 16:33:04 +0200

    Refine user flow in "remove" operation

--- src/commands/remove.py
+++ src/commands/remove.py
@@ -9,17 +9,25 @@ def run() -> None:
         print("Did not find any local pyra versions.")
         return
 
-    answer = utils.shell_utils.pretty_input(
-        f"Which version should be removed?", [v.as_str() for v in local_pyra_versions]
-    )
-    try:
-        version_to_be_removed = Version(answer)
-    except AssertionError:
-        utils.shell_utils.pretty_print(f'Invalid answer "{answer}"')
-        return
-    if version_to_be_removed not in local_pyra_versions:
-        utils.shell_utils.pretty_print(f'Invalid version "{version_to_be_removed.as_str()}"')
-        return
+    while True:
+        answer = utils.shell_utils.pretty_input(
+            f"Which version should be removed?",
+            [v.as_str() for v in local_pyra_versions] + ["abort"],
+        )
+        if answer == "abort":
+            utils.shell_utils.pretty_print("aborted by user")
+            return
+        try:
+            version_to_be_removed = Version(answer)
+        except AssertionError:
+            utils.shell_utils.pretty_print(f'Invalid answer "{answer}"')
+            continue
+        if version_to_be_removed in local_pyra_versions:
+            break
+        else:
+            utils.shell_utils.pretty_print(
+                f'Invalid version "{version_to_be_removed.as_str()}" (not installed locally)'
+            )
 
     tasks.remove_version.remove_version(version_to_be_removed)
     utils.shell_utils.pretty_print("done!", color="green")
