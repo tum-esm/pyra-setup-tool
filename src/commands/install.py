605e75858a23b16aaf9d3a9cf3961fb6d6d7071d
Moritz Makowski <moritz.makowski@tum.de>
2025-10-17 16:39:55 +0200

    Refine user flow in "install" operation

--- src/commands/install.py
+++ src/commands/install.py
@@ -15,22 +15,29 @@ def run(prerelease: bool = False) -> None:
     local_pyra_versions = tasks.find_versions.get_local_versions()
     remote_pyra_versions = tasks.find_versions.get_remote_versions(prerelease=prerelease)
     if len(remote_pyra_versions) == 0:
-        print("Did not find any remote pyra versions.")
+        utils.shell_utils.pretty_print("Did not find any released pyra versions.", color="red")
         return
 
-    answer = utils.shell_utils.pretty_input(
-        f"Which version should be installed?",
-        [v.as_str() for v in remote_pyra_versions],
-    )
-    try:
-        version_to_be_installed = Version(answer)
-    except AssertionError:
-        utils.shell_utils.pretty_print(f'Invalid answer "{answer}"')
-        return
+    while True:
+        answer = utils.shell_utils.pretty_input(
+            f"Which version should be installed?",
+            [v.as_str() for v in remote_pyra_versions] + ["abort"],
+        )
+        if answer == "abort":
+            utils.shell_utils.pretty_print("aborted by user")
+            return
+        try:
+            version_to_be_installed = Version(answer)
+        except AssertionError:
+            utils.shell_utils.pretty_print(f'Invalid answer "{answer}"')
+            continue
 
-    if version_to_be_installed not in remote_pyra_versions:
-        utils.shell_utils.pretty_print(f'Invalid version "{version_to_be_installed.as_str()}"')
-        return
+        if version_to_be_installed in remote_pyra_versions:
+            break
+        else:
+            utils.shell_utils.pretty_print(
+                f'Invalid version "{version_to_be_installed.as_str()}" (not in released versions)'
+            )
 
     if version_to_be_installed in local_pyra_versions:
         utils.shell_utils.pretty_print(
