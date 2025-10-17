d00387f1b5877e3079cc05128fae06735b51f5af
Moritz Makowski <moritz.makowski@tum.de>
2025-10-17 16:35:38 +0200

    Refine user flow in "rollback" operation

--- src/commands/rollback.py
+++ src/commands/rollback.py
@@ -11,20 +11,30 @@ def run() -> None:
     """
 
     local_pyra_versions = tasks.find_versions.get_local_versions()
-    answer = utils.shell_utils.pretty_input(
-        f"Which version should be rolled back to?",
-        [v.as_str() for v in local_pyra_versions],
-    )
-    try:
-        version_to_be_used = Version(answer)
-    except AssertionError:
-        utils.shell_utils.pretty_print(f'Invalid answer "{answer}"')
+    if len(local_pyra_versions) == 0:
+        print("Did not find any local pyra versions.")
         return
-    if version_to_be_used not in local_pyra_versions:
-        utils.shell_utils.pretty_print(f'Invalid version "{version_to_be_used.as_str()}"')
-        return
+
+    while True:
+        answer = utils.shell_utils.pretty_input(
+            f"Which version should be rolled back to?",
+            [v.as_str() for v in local_pyra_versions] + ["abort"],
+        )
+        if answer == "abort":
+            utils.shell_utils.pretty_print("aborted by user")
+            return
+        try:
+            version_to_be_used = Version(answer)
+        except AssertionError:
+            utils.shell_utils.pretty_print(f'Invalid answer "{answer}"')
+            continue
+        if version_to_be_used in local_pyra_versions:
+            break
+        else:
+            utils.shell_utils.pretty_print(
+                f'Invalid version "{version_to_be_used.as_str()}" (not installed locally)'
+            )
 
     tasks.install_version.install_version(version_to_be_used)
-    print("No migration, on rollbacks")
-
+    utils.shell_utils.pretty_print("No migration, on rollbacks")
     utils.shell_utils.pretty_print("done!", color="green")
